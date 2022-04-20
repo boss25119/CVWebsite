36f72f7f28378246866eff910db9475e24de1437
baongoc25111999@gmail.com <17071391@isvnu.vn>
2022-04-20 09:19:06 +0700

    About me

---
+++ app.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const hamburgerMenu = document.getElementsByClassName('hamburger');
+const mobileMenu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header .container');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+    mobileMenu.classList.toggle('active')
+})
